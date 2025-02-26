479b304d70223fd1244edb40db9ba38710937e33
sebavr-home <srvaldivia@gmail.com>
2025-02-26 17:58:36 -0300

    Avances

--- R/find_shapes.R
+++ R/find_shapes.R
@@ -1,3 +1,17 @@
+#' @title Find Shapefiles
+#'
+#' @descriptio
+#' Find files within a path which end with ".shp"
+#'
+#' @param path Path where to find th files.
+#'
+#' @returns Character vector with shapefile names
+#' @export find_shapes
+#'
+#' @examples
+#' find_shapes("path/to/your/location")
+
+
 find_shapes <- function(path) {
 
   shapes <- list.files(path = path, pattern = "\\.shp$")
---
+++ tests/testthat/test-find_shapes.R
@@ -0,0 +1,3 @@
+test_that("find shapes works", {
+  find_shapes(getwd())
+})
