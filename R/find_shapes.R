d4e6b03db5c80ac0ff89f8af2a53cad1d2627802
sebavr-ocuc <srvaldivia@gmail.com>
2025-03-07 17:24:16 -0300

    Add find_shapes()

--- R/find_shapes.R
+++ R/find_shapes.R
@@ -1,3 +1,22 @@
+#' Shapefile (.shp) finder
+#'
+#' `find_shapes()` searches Shapefiles existing within an existing folder o directory.
+#'
+#' @param path `character`. Path to a specific folder.
+#' @param full `logical`. If `FALSE` (the default) the function `find_shapes()` returns only the base file name of each Shapefile found. If `TRUE` it returns the full path name to every Shapefile
+#'
+#' @returns A `character` vector containing the names of the Shapefiles found in the folder path.
+#' @export
+#'
+#' @examples
+#' # Error message when using an inexisting path
+#' \dontrun{
+#' find_shapes(path = "github/project/data")
+#'
+#' # Error in `find_shapes()`:
+#' # ✖ The path "github/project/data" doesn't exist.
+#' # ℹ Make sure to use an existing folder path.
+#' }
 find_shapes <- function(path, full = FALSE) {
   if (missing(path) || is.null(path) || path == "" || trimws(path) == "") {
     cli::cli_abort(c("x" = "Argument {.code path} is missing or empty.",
---
+++ tests/testthat/test-find_shapes.R
@@ -0,0 +1,7 @@
+test_that(
+  "find_shapes()", {
+    expect_equal(
+      find_shapes(path = "C:/Users/Elite Center/Desktop"),
+      character(0))
+  }
+)
