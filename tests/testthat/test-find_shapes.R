c9767ae7188db27280258a3d08b442176823e9eb
sebavr-ocuc <srvaldivia@gmail.com>
2025-03-18 13:51:29 -0300

    Update test for find_shapes()

--- tests/testthat/test-find_shapes.R
+++ tests/testthat/test-find_shapes.R
@@ -1,12 +1,19 @@
-test_that("find_shapes() TESTING", {
+test_that("find a shapefile inside a directory using find_shapes()", {
   expect_equal(find_shapes(path = system.file("extdata", package = "milcao")),
                "estaciones_meteorologicas.shp")
-  # expect_equal(find_shapes(path = system.file("extdata", package = "milcao"), full = TRUE),
-  #              "C:/Users/Elite Center/Desktop/github/milcao/inst/extdata/estaciones_meteorologicas.shp")
-  expect_equal(find_shapes(path = system.file("extdata", package = "milcao"), subfolders = TRUE),
+  }
+)
+
+test_that("warning message when a shapefile is not found using find_shapes()", {
+  expect_equal(tryCatch(expr = find_shapes(path = system.file("extdata/datasets", package = "milcao")),
+                        error = function(e) "Error in `find_shapes()`:"),
+               "Error in `find_shapes()`:")
+  }
+)
+
+test_that("use subfolder argument to search in all existing subfolders", {
+  expect_equal(find_shapes(path = system.file("extdata", package = "milcao"),
+                           subfolders = TRUE),
                c("boundaries/dpa_comunas.shp", "boundaries/dpa_provincias.shp", "boundaries/dpa_regiones.shp", "estaciones_meteorologicas.shp"))
-  expect_equal(tryCatch(expr = find_shapes(path = "github/project/data"),
-                        error = function(e) NA_character_),
-               NA_character_)
   }
 )
