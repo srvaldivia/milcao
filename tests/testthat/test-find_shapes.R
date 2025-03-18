9fdce0b6275c3b65cc3743ed6b7c660c22b65ab7
sebavr-ocuc <srvaldivia@gmail.com>
2025-03-18 13:32:19 -0300

    Update test for find_shapes()

--- tests/testthat/test-find_shapes.R
+++ tests/testthat/test-find_shapes.R
@@ -1,8 +1,12 @@
-test_that(
-  "find_shapes() TESTING", {
-    expect_equal(
-      tryCatch(expr = find_shapes(path = "github/project/data"),
-               error = function(e) NA_character_),
-      NA_character_)
+test_that("find_shapes() TESTING", {
+  expect_equal(find_shapes(path = system.file("extdata", package = "milcao")),
+               "estaciones_meteorologicas.shp")
+  # expect_equal(find_shapes(path = system.file("extdata", package = "milcao"), full = TRUE),
+  #              "C:/Users/Elite Center/Desktop/github/milcao/inst/extdata/estaciones_meteorologicas.shp")
+  expect_equal(find_shapes(path = system.file("extdata", package = "milcao"), subfolders = TRUE),
+               c("boundaries/dpa_comunas.shp", "boundaries/dpa_provincias.shp", "boundaries/dpa_regiones.shp", "estaciones_meteorologicas.shp"))
+  expect_equal(tryCatch(expr = find_shapes(path = "github/project/data"),
+                        error = function(e) NA_character_),
+               NA_character_)
   }
 )
